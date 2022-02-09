93dcecf1367b17c745f20f2b862c19c82d8241ce
Karen Sepulveda Garcia <karennads10@gmail.com>
2022-02-08 23:16:48 -0500

    implement config module and setting by enviroments

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,4 +1,15 @@
 import { Module } from '@nestjs/common';
 
-@Module({})
+import { CustomersController } from './controllers/customers.controller';
+import { CustomersService } from './services/customers.service';
+import { UsersController } from './controllers/users.controller';
+import { UsersService } from './services/users.service';
+
+import { ProductsModule } from 'src/products/products.module';
+
+@Module({
+  imports: [ProductsModule],
+  controllers: [CustomersController, UsersController],
+  providers: [CustomersService, UsersService],
+})
 export class UsersModule {}
