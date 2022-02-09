15c2e7a5aea7cb6193920772b28575d751e803a1
Karen Sepulveda Garcia <karennads10@gmail.com>
2022-02-08 23:38:11 -0500

    implement configuration validation in .env files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,18 +1,26 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductsModule } from './products/products.module';
 import { UsersModule } from './users/users.module';
 import { DatabaseModule } from './database/database.module';
-
 import { enviroments } from './enviroments';
+import config from './config';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       envFilePath: enviroments[process.env.NODE_ENV] || '.env',
+      load: [config],
       isGlobal: true,
+      validationSchema: Joi.object({
+        API_KEY: Joi.number().required(),
+        DATABASE_NAME: Joi.string().required(),
+        DATABASE_PORT: Joi.number().required(),
+      })
     }),
     ProductsModule,
     UsersModule,
